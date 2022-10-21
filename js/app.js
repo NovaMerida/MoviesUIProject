c97f1b9d1473074e32b6bfb981cde6c7902ac786
NovaMerida <novareymerida@gmail.com>
2022-10-21 00:29:19 -0400

    full update

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ const input =document.getElementById('movie_search');
 
 
 
-
 const createElements = (x) => {
+
     var col = document.createElement('div');
     col.className = 'col s3 m3';
 
@@ -29,20 +29,40 @@ const createElements = (x) => {
     overlay2.className="overlay2";
     col.appendChild(overlay2);
 
+    
+    
     let text = document.createElement('div');
-    text.className = 'text'
-    text.innerHTML = `<h5>${x.original_title}</h5>`
+    text.className= "text"
+    text.innerHTML = `<h5>${x.original_title}</h5> <div class="card-panel" style="background:#810000;color:white;"> ${(x.vote_average)} <span id=stars>${getStars(x.vote_average)}</span> </div>`
     overlay.appendChild(text);
+
+    let text2 = document.createElement('div');
+    text2.className= "text2"
+    text2.innerHTML = `${x.overview}`
+    overlay2.appendChild(text2);
+
     row_body.appendChild(col);
-    
-    
-
-
     
 }
 
 
+function getStars(rating) {
 
+  rating = Math.round(rating * 2) / 2;
+  new_rating = (rating/10)*5;
+  let output = [];
+
+  for (var i = new_rating; i >= 1; i--)
+    output.push('<i class="fa fa-star" aria-hidden="true" style="color: white;"></i>&nbsp;');
+
+  if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: white;"></i>&nbsp;');
+
+  for (let i = (5 - new_rating); i >= 1; i--)
+    output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: white;"></i>&nbsp;');
+
+  return output.join('');
+
+}
 
 const getMovies=() =>{
     fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=${Math.floor(Math.random() * 100) + 1}`)
@@ -89,3 +109,4 @@ const fetchName=()=>{
     window.onload = function() {
         getMovies()
       };
+      
